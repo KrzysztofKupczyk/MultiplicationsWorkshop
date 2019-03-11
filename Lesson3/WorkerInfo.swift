148fbd23e2e9cca238a51a146996bfa480cdb0b7
Rafal Szastok <rafal.szastok@performgroup.com>
2019-03-11 11:31:59 +0100

    Workshop preparations part 2

---
+++ Lesson3/WorkerInfo.swift
@@ -0,0 +1,39 @@
+//
+//  Created by Rafal Szastok on 11/03/2019
+//  Copyright © 2017 Perform. All rights reserved.
+//
+	
+
+import Foundation
+
+final class WorkerInfo {
+    enum Position {
+        case qaDeveloper
+        case iosDeveloper
+        case scrumHamster
+        case designer
+        case productOwner
+    }
+
+    let firstName: String?
+    let lastName: String?
+    let birthDate: Date?
+    let workFloor: UInt8
+    let position: Position?
+
+    init () {
+        firstName = nil
+        lastName = nil
+        birthDate = nil
+        workFloor = 6
+        position = nil
+    }
+
+    var fullName: String? {
+        return nil
+    }
+
+    var needAccessToRepository: Bool {
+        return false
+    }
+}
---
+++ MultiplicationsTests/Lesson3/WorkerInfoTests.swift
@@ -0,0 +1,28 @@
+//
+//  Created by Rafal Szastok on 11/03/2019
+//  Copyright © 2017 Perform. All rights reserved.
+//
+
+import XCTest
+@testable import Multiplications
+
+final class WorkerInfoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testFullName() {
+        // Write tests that check if user "Jan" Kowalski" has "Jan Kowalski as output.
+    }
+
+    func testIsHasDeveloper() {
+        // Write tests that check if for QA and developers userInfo returns TRUE.
+        // For other cases it should return FALSE.
+    }
+
+}
